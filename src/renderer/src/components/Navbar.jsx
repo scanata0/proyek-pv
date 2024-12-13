7823e29c434d4bd951d8e37a94e46435262ba4c3
5val <valensiaclarins555@gmail.com>
2024-12-13 15:11:16 +0700

    test2

--- src/renderer/src/components/Navbar.jsx
+++ src/renderer/src/components/Navbar.jsx
@@ -1,32 +1,74 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Box, TextField, IconButton } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function Navbar() {
+   const [anchorEl, setAnchorEl] = React.useState(null);
+   const open = Boolean(anchorEl);
+   const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+   };
+   const handleClose = () => {
+      setAnchorEl(null);
+   };
+
    return(
       <AppBar position="fixed" sx={{ backgroundColor: '#00b140' }}>
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+         <Box sx={{display: 'flex', gap: 3, flexGrow: 1}}>
+        <Typography variant="h6">
           Tokotokoan
         </Typography>
 
         {/* Navigation Links */}
-        <Box sx={{ display: 'flex', gap: 2 }}>
+        <Box sx={{ display: 'flex', gap: 2, marginLeft: 2 }}>
           <Button color="inherit">Home</Button>
-          <Button color="inherit">Shop</Button>
-          <Button color="inherit">Categories</Button>
+          <div>
+            <Button
+            id="basic-button"
+            color='inherit'
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            >
+            Kategori
+            </Button>
+            <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+               'aria-labelledby': 'basic-button',
+            }}
+            >
+            <MenuItem onClick={handleClose}>Elektronik</MenuItem>
+            <MenuItem onClick={handleClose}>Fashion</MenuItem>
+            <MenuItem onClick={handleClose}>Kecantikan</MenuItem>
+            <MenuItem onClick={handleClose}>Kesehatan</MenuItem>
+            <MenuItem onClick={handleClose}>Makanan & Minuman</MenuItem>
+            </Menu>
+         </div>
           <Button color="inherit">Deals</Button>
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: 2 }}>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: 2, flexGrow: 1 }}>
           <TextField
             variant="outlined"
             size="small"
             placeholder="Search products, brands, or categories"
-            sx={{ backgroundColor: 'white', borderRadius: 1 }}
+            sx={{ backgroundColor: 'white', borderRadius: 1, width: '700px' }}
           />
           <IconButton color="inherit" sx={{ marginLeft: 1 }}>
             <SearchIcon />
           </IconButton>
+        </Box>
+        <Box sx={{ display: 'flex', gap: 2, flexGrow: 1, justifyContent: 'end' }}>
+          <Button color="inherit">Sign Up</Button>
+          <Button color="inherit">Log In</Button>
         </Box>
       </Toolbar>
     </AppBar>
